f4bbf699efc31492bd8c6fdcd9980393431d0ee4
Dan Schultzer <dan@dreamconception.com>
2018-04-22 11:02:08 -0700

    Test for consecutive tags issues in plain text parser

--- test/premailex/html_to_plain_text_test.exs
+++ test/premailex/html_to_plain_text_test.exs
@@ -13,6 +13,7 @@ defmodule Premailex.HTMLToPlainTextTest do
   <p><a href="http://example.com">HTTP://EXAMPLE.COM</a></p>
   <p><a href="http://example.com"></a></p>
   <p><span>Test</span> some very long paragraph with <strong>bold</strong> and <i>italic</i> text, including an <a href="http://example.com">inline link</a>. This should break up on multiple lines.</p>
+  <p><span>Test</span> <strong>consecutive</strong> <i>tags</i>.</p>
 
   <img href="logo.png" alt="Test image" /><br/>
   <img href="logo.png" /><br/>
@@ -84,6 +85,8 @@ defmodule Premailex.HTMLToPlainTextTest do
   Test some very long paragraph with bold and italic text,
   including an inline link (http://example.com). This should break
   up on multiple lines.
+
+  Test consecutive tags.
 
   Test image
 
