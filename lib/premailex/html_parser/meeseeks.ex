7e7320c47182c9fd1697639a3a6a7f10988edfa6
Dan Schultzer <dan@dreamconception.com>
2018-04-25 07:58:51 -0700

    Fix formatting

--- lib/premailex/html_parser/meeseeks.ex
+++ lib/premailex/html_parser/meeseeks.ex
@@ -1,52 +1,52 @@
 if Code.ensure_loaded?(Meeseeks) do
-defmodule Premailex.HTMLParser.Meeseeks do
-  @moduledoc """
-  API connection with Meeseeks
-  """
+  defmodule Premailex.HTMLParser.Meeseeks do
+    @moduledoc """
+    API connection with Meeseeks
+    """
 
-  require Logger
-  import Meeseeks.CSS
-  alias Premailex.HTMLParser
-  alias Meeseeks.Selector.CSS.Parser.ParseError
+    require Logger
+    import Meeseeks.CSS
+    alias Premailex.HTMLParser
+    alias Meeseeks.Selector.CSS.Parser.ParseError
 
-  @doc false
-  @spec parse(String.t()) :: HTMLParser.html_tree()
-  def parse(html) do
-    html
-    |> Meeseeks.parse()
-    |> Meeseeks.tree()
-    |> case do
-      [html] -> html
-      html -> html
+    @doc false
+    @spec parse(String.t()) :: HTMLParser.html_tree()
+    def parse(html) do
+      html
+      |> Meeseeks.parse()
+      |> Meeseeks.tree()
+      |> case do
+        [html] -> html
+        html -> html
+      end
     end
-  end
 
-  @doc false
-  @spec all(HTMLParser.html_tree(), String.t()) :: [HTMLParser.html_tree()]
-  def all(tree, selector) do
-    try do
+    @doc false
+    @spec all(HTMLParser.html_tree(), String.t()) :: [HTMLParser.html_tree()]
+    def all(tree, selector) do
+      try do
+        tree
+        |> Meeseeks.all(css("#{selector}"))
+        |> Enum.map(&Meeseeks.tree/1)
+      rescue
+        e in ParseError ->
+          Logger.warn("Meeseeks CSS ParseError: " <> e.message)
+          []
+      end
+    end
+
+    @doc false
+    @spec to_string(HTMLParser.html_tree()) :: String.t()
+    def to_string(tree) do
       tree
-      |> Meeseeks.all(css("#{selector}"))
-      |> Enum.map(&Meeseeks.tree/1)
-    rescue
-      e in ParseError ->
-        Logger.warn("Meeseeks CSS ParseError: " <> e.message)
-        []
+      |> Meeseeks.parse()
+      |> Meeseeks.html()
     end
-  end
 
-  @doc false
-  @spec to_string(HTMLParser.html_tree()) :: String.t()
-  def to_string(tree) do
-    tree
-    |> Meeseeks.parse()
-    |> Meeseeks.html()
+    @doc false
+    @spec text(HTMLParser.html_tree()) :: String.t()
+    def text(text) when is_binary(text), do: text
+    def text(list) when is_list(list), do: Enum.map_join(list, "", &text/1)
+    def text({_element, _attrs, children}), do: text(children)
   end
-
-  @doc false
-  @spec text(HTMLParser.html_tree()) :: String.t()
-  def text(text) when is_binary(text), do: text
-  def text(list) when is_list(list), do: Enum.map_join(list, "", &text/1)
-  def text({_element, _attrs, children}), do: text(children)
-end
 end
