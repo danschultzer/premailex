8eec8c92a24c421ac2afc9bc3e01476466afda7d
Dan Schultzer <1254724+danschultzer@users.noreply.github.com>
2022-03-08 16:30:22 -0800

    Clean up

--- mix.exs
+++ mix.exs
@@ -1,7 +1,6 @@
 defmodule Premailex.Mixfile do
   use Mix.Project
 
-  @source_url "https://github.com/danschultzer/premailex"
   @version "0.3.13"
 
   def project do
@@ -11,6 +10,7 @@ defmodule Premailex.Mixfile do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      xref: [exclude: [:certifi, :httpc, Meeseeks, Meeseeks.Document, Meeseeks.Selector.CSS, :ssl_verify_hostname]],
 
       # Hex
       description: "Add inline styling to your HTML emails, and transform them to text",
@@ -18,17 +18,7 @@ defmodule Premailex.Mixfile do
 
       # Docs
       name: "Premailex",
-      docs: docs(),
-      xref: [
-        exclude: [
-          :certifi,
-          :httpc,
-          Meeseeks,
-          Meeseeks.Document,
-          Meeseeks.Selector.CSS,
-          :ssl_verify_hostname
-        ]
-      ]
+      docs: docs()
     ]
   end
 
@@ -42,8 +32,8 @@ defmodule Premailex.Mixfile do
     [
       {:floki, "~> 0.19"},
 
-      {:certifi, ">= 0.0.0", optional: true},
       {:meeseeks, "~> 0.11", optional: true},
+      {:certifi, ">= 0.0.0", optional: true},
       {:ssl_verify_fun, ">= 0.0.0", optional: true},
 
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
@@ -53,25 +43,25 @@ defmodule Premailex.Mixfile do
 
   defp package do
     [
-      maintainers: ["Dan Shultzer"],
+      maintainers: ["Dan Schultzer"],
       licenses: ["MIT"],
       links: %{
         "GitHub" => "https://github.com/danschultzer/premailex",
         "Sponsor" => "https://github.com/sponsors/danschultzer"
       },
-      files: ~w(lib LICENSE mix.exs README.md CHANGELOG.md)
+      files: ~w(lib LICENSE mix.exs README.md)
     ]
   end
 
   defp docs do
     [
       source_ref: "v#{@version}",
-      main: "readme",
+      main: "README",
       canonical: "http://hexdocs.pm/premailex",
-      source_url: @source_url,
+      source_url: "https://github.com/danschultzer/premailex",
       extras: [
-        "README.md",
-        "CHANGELOG.md"
+        "README.md": [filename: "README"],
+        "CHANGELOG.md": [filename: "CHANGELOG"]
       ]
     ]
   end
