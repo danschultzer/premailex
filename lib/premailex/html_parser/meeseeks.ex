e1f27fc1bc30bd1a4b432862b3736c52110f8ffd
Agathe Lenclen <agathe@bitcrowd.net>
2025-08-06 11:37:03 +0200

    Fix `struct Meeseeks.Error is undefined` when meeseeks deps is missing
    When opting to use the floki html parser dependency, and not listing
    meeseeks in a project using premailex, the following warning comes up:
    
    ```
    ==> premailex
    Compiling 10 files (.ex)
        warning: struct Meeseeks.Error is undefined (module Meeseeks.Error is not available or is yet to be defined)
        │
     33 │     e in Meeseeks.Error ->
        │       ~
        │
        └─ lib/premailex/html_parser/meeseeks.ex:33:7: Premailex.HTMLParser.Meeseeks.all/2
    ```
    
    Since the dependency `meeseeks` is optional, we can wrap the module with
    ensure_loaded to remove this warning.
    
    # Conflicts:
    #	lib/premailex/html_parser/meeseeks.ex

--- lib/premailex/html_parser/meeseeks.ex
+++ lib/premailex/html_parser/meeseeks.ex
@@ -1,66 +1,68 @@
-defmodule Premailex.HTMLParser.Meeseeks do
-  @moduledoc false
+if Code.ensure_loaded?(Meeseeks) do
+  defmodule Premailex.HTMLParser.Meeseeks do
+    @moduledoc false
 
-  require Logger
+    require Logger
 
-  alias Meeseeks.{Document, Selector.CSS}
-  alias Premailex.HTMLParser
+    alias Meeseeks.{Document, Selector.CSS}
+    alias Premailex.HTMLParser
 
-  @behaviour HTMLParser
+    @behaviour HTMLParser
 
-  @impl true
-  @doc false
-  def parse(html) do
-    html
-    |> Meeseeks.parse()
-    |> Meeseeks.tree()
-    |> case do
-      [html] -> html
-      html -> html
+    @impl true
+    @doc false
+    def parse(html) do
+      html
+      |> Meeseeks.parse()
+      |> Meeseeks.tree()
+      |> case do
+        [html] -> html
+        html -> html
+      end
     end
-  end
 
-  @impl true
-  @doc false
-  def all(tree, selector) do
-    selector = CSS.compile_selectors(selector)
+    @impl true
+    @doc false
+    def all(tree, selector) do
+      selector = CSS.compile_selectors(selector)
 
-    tree
-    |> Meeseeks.parse(:tuple_tree)
-    |> Meeseeks.all(selector)
-    |> Enum.map(&Meeseeks.tree/1)
-  rescue
-    e in Meeseeks.Error ->
-      Logger.warning("Meeseeks error: " <> inspect(e))
-      []
-  end
+      tree
+      |> Meeseeks.parse(:tuple_tree)
+      |> Meeseeks.all(selector)
+      |> Enum.map(&Meeseeks.tree/1)
+    rescue
+      e in Meeseeks.Error ->
+        Logger.warning("Meeseeks error: " <> inspect(e))
+        []
+    end
 
-  @impl true
-  @doc false
-  def to_string(tree) do
-    tree
-    |> Meeseeks.parse(:tuple_tree)
-    |> Meeseeks.html()
-  end
+    @impl true
+    @doc false
+    def to_string(tree) do
+      tree
+      |> Meeseeks.parse(:tuple_tree)
+      |> Meeseeks.html()
+    end
 
-  @impl true
-  @doc false
-  def text(text) when is_binary(text), do: text
-  def text(list) when is_list(list), do: Enum.map_join(list, "", &text/1)
-  def text({:comment, _text}), do: ""
-  def text({_element, _attrs, children}), do: text(children)
+    @impl true
+    @doc false
+    def text(text) when is_binary(text), do: text
+    def text(list) when is_list(list), do: Enum.map_join(list, "", &text/1)
+    def text({:comment, _text}), do: ""
+    def text({_element, _attrs, children}), do: text(children)
 
-  @impl true
-  @doc false
-  def filter(tree, selector) do
-    selector = CSS.compile_selectors(selector)
-    tree = Meeseeks.parse(tree, :tuple_tree)
+    @impl true
+    @doc false
+    def filter(tree, selector) do
+      selector = CSS.compile_selectors(selector)
+      tree = Meeseeks.parse(tree, :tuple_tree)
 
-    tree
-    |> Meeseeks.all(selector)
-    |> Enum.reduce(tree, fn e, acc ->
-      Document.delete_node(acc, e.id)
-    end)
-    |> Meeseeks.tree()
+      tree
+      |> Meeseeks.all(selector)
+      |> Enum.reduce(tree, fn e, acc ->
+        Document.delete_node(acc, e.id)
+      end)
+      |> Meeseeks.tree()
+    end
   end
 end
